742bb463234ba0097c5285b2c1b2785b623d61ad
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2024-03-01 21:09:23 +0100

    Add files via upload

---
+++ 6.3. Glosariusz.py
@@ -0,0 +1,11 @@
+# Słownik pojęć związanych z programowaniem i pythonem
+Glosariusz = {
+    'ciąg tekstowy': 'Seria znaków zamkniętych znakami cytowania',
+    'lista': 'zbiór elementów ułożonych w określonej kolejności którą możemy edytować, \
+zamknięty nawiasami kwadratowymi i rozdzielony przecinkami',
+    'pętla for': 'pozwala wykonać konkretną serię zadań na wszystkich elementach',
+    'krotka': 'lista, której nie można edytować, zamknięta nawiasami okrągłymi',
+}
+print('ciąg tekstowy:')
+print(Glosariusz['ciąg tekstowy'])
+print('\n')
