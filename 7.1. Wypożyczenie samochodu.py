e23da584a7bae96c864cda8f195686510a9cc78c
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2024-03-03 16:24:20 +0100

    Add files via upload

---
+++ 7.1. Wypożyczenie samochodu.py
@@ -0,0 +1,3 @@
+# wypożyczenie smochodu
+samochód = input('Dzień dobry,\njakiej marki samochód chciałbyś wypożyczyć?\n')
+print(f'Już sprawdzam czy mamy dostępny samochód marki {samochód}')
---
+++ 7.2. Stolik w restauracji.py
@@ -0,0 +1,5 @@
+ile_osób = int(input('Dzień dobry, na ile osób chciałby Pan/Pani zarezerwować stolik?\n'))
+if ile_osób > 8:
+    print('Musi Pan/Pani zaczekać chwilę na stolik.')
+else:
+    print('Stolik jest gotowy')
