2c80a6ea18e3edb0c525afe68e15cd9a28902665
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2024-03-03 17:06:26 +0100

    Add files via upload

---
+++ 7.3. Wielokrotność dziesięciu.py
@@ -0,0 +1,6 @@
+liczba = int(input('Proszę o podanie liczby, a ja sprawdzę czy liczba ta\
+ jest wielokrotnością liczbt 10\n'))
+if liczba % 10 == 0:
+    print('Liczba jest wielokrotnością liczby 10')
+else:
+    print('liczba nie jest wielorotnością liczby 10')
