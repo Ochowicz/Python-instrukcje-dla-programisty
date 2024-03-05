a79db84c71364a0f9d1e9962fbbe61c8fc51f726
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2024-03-05 23:12:49 +0100

    Add files via upload

---
+++ 8.1. Komunikat.py
@@ -0,0 +1,4 @@
+def display_messafes():
+    print('W tym rozdziale uczę się o funkcjach')
+
+display_messafes()
---
+++ 8.2. Ulubiona książka.py
@@ -0,0 +1,4 @@
+def favourite_book(title):
+    print(f'Jedną z moich ulubionych książek jest {title.title()}.')
+
+favourite_book('mały książe')
